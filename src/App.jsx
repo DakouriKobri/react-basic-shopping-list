89b5a7f9f18287e5c281ad5cf7f26648285c1961
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-04-08 11:35:41 +0200

    Build the JSX structutre of App.jx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+// Project Imports
 import './App.css';
+import groceryCartImage from './assets/grocery-cart.png';
 
 function App() {
   return (
-    <>
-      <h1>Shopping List</h1>
-    </>
+    <main className="App">
+      <div>
+        <div>
+          <h4 className="success">You&apos;re done</h4>
+          <div className="header">Shopping List</div>
+          <img src={groceryCartImage} alt="Abstract grocery cart" />
+          <input type="text" placeholder="Add an Item" className="item-input" />
+        </div>
+        <ul>
+          <li>
+            <div className="container">
+              <input type="checkbox" />
+              <p>Carrots</p>
+            </div>
+            <div>
+              <button className="remove-button">&times;</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </main>
   );
 }
 
